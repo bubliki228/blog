f6c3fb9cbfe995b801ced5c2bd8eebc08c789ed1
vedulix <us.do.to@gmail.com>
2022-05-17 14:05:18 +0300

    some changes

--- read.py
+++ read.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 import telebot
+import markdown
+bot = telebot.TeleBot("1451808041:AAEbQKwW70Kd-oItc7CdYADNnZ2tOhp3N5k", skip_pending=True)
 
-f = open('_posts/2022-05-07-school-ending.md', 'r', encoding="utf8")
+stroka='_posts/school-ending.md'
 
+
+f = open(stroka, 'r', encoding="utf8")
 text = f.read()
 text = text[text.find('---', 2)+5:]
-
-import markdown
 html = markdown.markdown(text).replace('<p>', '').replace('</p>', '\n')
 print(type(html), html)
-"""with open('Picnic.md', 'r') as f:
-    text = f.read()
-    html = markdown.markdown(text)
-"""
-bot = telebot.TeleBot("1451808041:AAEbQKwW70Kd-oItc7CdYADNnZ2tOhp3N5k"  , skip_pending=True)
+
 
 
 
@@ -30,5 +28,3 @@ if __name__ == '__main__':
     except Exception as ex: 
         print('Возникла осечка в работе программы.\nПричина: ')
         print(ex, type(ex), sep='\n')
-        
-
